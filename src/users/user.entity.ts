f56735394c91cf396d708a3aa7ecfcb27656ad0b
Ousmane Fall <ousmane.fall@akilee.tech>
2024-03-11 19:09:31 +0000

    updatig data

---
+++ src/users/user.entity.ts
@@ -0,0 +1,18 @@
+import { AfterInsert, Entity, Column, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password: string;
+
+  @AfterInsert()
+  logInsert() {
+    console.log("Inserted User with id: ", this.id);
+  }
+}
